48f06f160ddca116de132c97fa422c6369c8840d
Dion2011q <dion.p.dogger@protonmail.com>
2025-07-22 17:43:48 +0200

    hero + sections

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 // year footer
 const yearEl = document.getElementById('year');
 if (yearEl) yearEl.textContent = new Date().getFullYear();
+
+
+
+// Hamburger menu functionality
+$(document).ready(function(){
+	$('#nav-icon3').click(function(){
+		$(this).toggleClass('open');
+	});
+});
