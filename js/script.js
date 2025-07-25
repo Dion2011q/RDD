76c45086843fc0c653522956200699211c4eca89
Dion2011q <dion.p.dogger@protonmail.com>
2025-07-25 13:47:44 +0200

    veel 2

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
 // year footer
 const yearEl = document.getElementById('year');
 if (yearEl) yearEl.textContent = new Date().getFullYear();
-
-
-
-// Hamburger menu functionality
-$(document).ready(function(){
-	$('#nav-icon3').click(function(){
-		$(this).toggleClass('open');
-	});
-});
